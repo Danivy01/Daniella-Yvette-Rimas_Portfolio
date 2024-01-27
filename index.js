3cad4c8be217ad8b487234d653090df1237d1adf
RIMAS, DANIELLA YVETTE D. <daniellayvetterimas@gmail.com>
2024-01-27 14:07:43 +0800

    add contact form

--- index.js
+++ index.js
@@ -36,3 +36,49 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior.
+
+  var name = document.getElementById("name").value.trim();
+
+  console.log("Name:", name);
+  console.log("Name Validation:", validateName(name));
+
+  // var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+
+  // if (!name || !validateName(name)) {
+  //   alert("Please enter a valid name (at least 2 characters).");
+  // } else 
+  if (!email || !validateEmail(email)) {
+    alert("Please enter a valid email address.");
+  } else if (!message) {
+    alert("Please enter a message.");
+  } else {
+    var params = {
+      name: name,
+      email: email,
+      message: message
+    }
+
+    emailjs.send("service_x569wne","template_zd8dsv9", params)
+      .then(function (res) {
+        alert("Sent! " + res.status);
+        document.getElementById("form").reset();
+      });
+  }
+});
+
+
+function validateName(name) {
+  var namePattern = /^[A-Za-z]{2,}$/; // At least 2 alphabetical characters
+  return namePattern.test(name);
+}
+
+function validateEmail(email) {
+  var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
